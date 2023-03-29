7d1d63bbdf43aa984db8f049ff7a86c33e0371fd
VoThang-k1 <103491171+VoThang-k1@users.noreply.github.com>
2023-03-29 14:39:52 +0700

    Update deployProxy.js

--- scripts/deployProxy.js
+++ scripts/deployProxy.js
@@ -8,38 +8,54 @@ async function main() {
   console.log("Deploying contracts with th account:", deployer.address);
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
-//--deployImplementation
+/**
+ * deployImplementation
+ */
   const Implementation = await ethers.getContractFactory("Implementation");
   // const implementation = await Implementation.deploy();
   // console.log("implementation address:", implementation.address);
 
-//--deployProxy1967 set Implement
+/**
+ *deployProxy1967 set Implement
+ */
   // const Proxy = await ethers.getContractFactory("MyProxyBaseContract");
   // const proxy = await Proxy.deploy(process.env.IMPLEMENTATION,"0x");
   // console.log("Proxy address:", proxy.address);
 
-//--chay implement qua logic proxy
+/**
+ * Logic implement qua proxy
+ */
   implementation = Implementation.attach(process.env.PROXY1967);
   
-//--deploytoken
+/**
+  *deploytoken
+  */
   // const Token = await ethers.getContractFactory("Token");
   // const token = await Token.deploy(process.env.PROXY1967, "Thang","Than");
   // console.log("Token address:", token.address);
 
-//--deployHandler
+/**
+ * deployHandler
+ */
   // const Handler = await ethers.getContractFactory("AirdropHandler");
   // const handler = await Handler.deploy();
   // console.log("Handler address:", handler.address);
 
-//--set Airdrophandler
+/**
+  *set Airdrophandler
+  */
   // const setAirdrophandler = await implementation.setAirdropHandler(process.env.HANDLER);
   // await setAirdrophandler.wait();
 
-//--set WHITELIST
+/**
+  *set WHITELIST
+  */
   // const setWhitelist = await implementation.setWhitelist(process.env.WHITELIST);
   // await setWhitelist.wait();
 
-//--AIRDROP
+/**
+  *AIRDROP
+  */
   const airdrop = await implementation.Airdrop([process.env.RECEIVER_ADDRESS],2001);
   await airdrop.wait();
 
